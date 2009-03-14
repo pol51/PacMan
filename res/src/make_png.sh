0d72b66bcfd9b428c136c1eec21ac67ddb333870
Paul Labedan <plabedan@gmail.com>
2009-03-14 18:48:48 +0000

    script generation of tiles
    git-svn-id: http://ssx.labedan.fr/svn/PacMan@5 65f8c0d8-71a2-4076-a20d-31daf5a9014d

---
+++ res/src/make_png.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#pacman
+montage -background "transparent" pacman_right_*.svg -geometry 40x40 -tile 1x6 -matte -transparent "transparent" pacman_right.png
+montage -background "transparent" pacman_right_*.svg -flop -geometry 40x40 -tile 1x6 -matte -transparent "transparent" pacman_left.png
+montage -background "transparent" pacman_right_*.svg -rotate 90 -flop -geometry 40x40 -tile 1x6 -matte -transparent "transparent" pacman_down.png
+montage -background "transparent" pacman_right_*.svg -rotate 270 -geometry 40x40 -tile 1x6 -matte -transparent "transparent" pacman_up.png
+montage -background "transparent" pacman_{left,up,right,down}.png -geometry 40x240 -tile 4x1 -matte -transparent "transparent" ../pacman.png
+rm -f pacman_{left,up,right,down}.png
+
+#ghosts
+montage -background "transparent" ghost_blue_{left,up,right,down}.svg -geometry 40x40 -tile 4x1 -matte -transparent "transparent" ../ghost_blue.png
+montage -background "transparent" ghost_orange_{left,up,right,down}.svg -geometry 40x40 -tile 4x1 -matte -transparent "transparent" ../ghost_orange.png
+montage -background "transparent" ghost_pink_{left,up,right,down}.svg -geometry 40x40 -tile 4x1 -matte -transparent "transparent" ../ghost_ping.png
+montage -background "transparent" ghost_red_{left,up,right,down}.svg -geometry 40x40 -tile 4x1 -matte -transparent "transparent" ../ghost_red.png
